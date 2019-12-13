0fa0a5381f8a4bb95bea2525102432377b170f01
jidefakoya <jidefakoya@hotmail.co.uk>
2019-12-13 04:06:50 +0000

    Added tests for fizzbuz rules

---
+++ src/test/java/com/jfsoftware/FizzBuzzRuleTest.java
@@ -0,0 +1,31 @@
+package com.jfsoftware;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Rather than testing all the possible values in the range 1..100, the tests assume that some values would make the
+ * system behave in the same way. For example testing for the integer 15 is the same as testing for integer 90 for
+ * test in this class.
+ */
+class FizzBuzzRuleTest {
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {15, 90})
+    void ruleShouldReturnFizzBuzzForIntegersThatAreMultiplesOfFiveAndThree(int integer) {
+        FizzBuzzRule rule = new FizzBuzzRule(null);
+        assertThat(rule.apply(integer)).isEqualTo("FizzBuzz");
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 99})
+    void ruleShouldReturnIntegerIfNextRuleIsSetToNoMatchAndIntegerIsNotAMultipleOfFiveAndThree(int integer) {
+        FizzBuzzRule rule = new FizzBuzzRule(new NoMatchRule());
+        assertThat(rule.apply(integer)).isEqualTo(String.valueOf(integer));
+    }
+
+}
